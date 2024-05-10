fae5deacd52052f6ece5f1049574bd1d8665c9f5
everEvergolw <1092547508@qq.com>
2024-05-10 17:45:21 +0100

    design:wishlist backend api

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :wishlists, dependent: :destroy
+  has_many :wishlisted_properties, through: :wishlist, source: :user, dependent: :destroy
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+
+  namespace :api do
+
+    resources :wishlists, only:[:create, :destroy]
+  
+
+
+  end
+
 end
